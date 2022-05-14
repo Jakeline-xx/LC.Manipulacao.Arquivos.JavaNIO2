eb88aabb62aee60939d05e29d4faf21d6c168f5f
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2022-05-14 19:38:46 -0300

    add: leitura do arquivo csv e inserindo cada informacao em uma lista de objetos

---
+++ src/Ator.java
@@ -0,0 +1,56 @@
+public class Ator {
+    private String index;
+    private String year;
+    private int age;
+    private String name;
+    private String movie;
+
+    public Ator(){}
+    public Ator(String index, String year, int age, String name, String movie){
+        this.index = index;
+        this.year = year;
+        this.age = age;
+        this.name = name;
+        this.movie = movie;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String args[]){
+         Path path = Paths.get("C:/Projetos/BackEnd_LetsCode/LC.MaterialProjetoJava/male_oscar.csv");
+         readLine(path);
+    }
+
+    public static void readLine(Path file){
+        try(Stream<String> stream = Files.lines(file, StandardCharsets.UTF_8))
+        {
+            List<Ator> listaAtores = new ArrayList<Ator>();
+                stream.skip(1).forEach(line -> {
+                    String[] lineTeste = line.split(";");
+                    Arrays.toString(lineTeste);
+                    Ator ator = new Ator();
+                    ator.setIndex(lineTeste[0]);
+                    ator.setYear(lineTeste[1]);
+                    ator.setAge(Integer.parseInt(lineTeste[2]));
+                    ator.setName(lineTeste[3]);
+                    ator.setMovie(lineTeste[4]);
+                    listaAtores.add(ator);
+                });
+            System.out.println("FIM");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
