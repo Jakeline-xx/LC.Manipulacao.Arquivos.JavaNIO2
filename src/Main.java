857db95091c742488e9ffed0a24ed220abafdd35
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2022-05-15 01:10:26 -0300

    add: métodos utilizando java stream

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,54 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String args[]){
-        Path path = Paths.get("C:/Projetos/BackEnd_LetsCode/LC.MaterialProjetoJava/male_oscar.csv");
-        List<Ator> listaCompleta = new ArrayList<>();
-        listaCompleta = readLine(path);
+        Path pathMale = Paths.get("C:/Projetos/BackEnd_LetsCode/LC.MaterialProjetoJava/male_oscar.csv");
+        Path pathFemale = Paths.get("C:/Projetos/BackEnd_LetsCode/LC.MaterialProjetoJava/female_oscar.csv");
+
+        List<Ator> listMale = new ArrayList<>();
+        listMale = readData(pathMale);
+
+        List<Ator> listFemale = new ArrayList<>();
+        listFemale = readData(pathFemale);
+
+        //ATOR MAIS JOVEM
+        String youngerActor = getYougerActor(listMale);
+        System.out.println("Ator mais jovem premiado: " + youngerActor);
+
+        //ATRIZ MAIS PREMIADA
+        String actressMostAwarded = getMostAwarded(listFemale);
+        System.out.println("Atriz que mais vezes foi premiada: " + actressMostAwarded);
     }
 
-    public static List<Ator> readLine(Path path){
+    public static String getMostAwarded(List<Ator> actresses){
+         return actresses.stream()
+                .collect(Collectors.groupingBy(Ator::getName, Collectors.counting()))
+                 .entrySet()
+                 .stream()
+                 .max((Map.Entry<String, Long> e1, Map.Entry<String, Long> e2) -> e1.getValue()
+                         .compareTo(e2.getValue())).get().getKey();
+    }
+
+    public static String getYougerActor(List<Ator> actors){
+        return actors.stream()
+                .min(Comparator.comparingInt(Ator::getAge))
+                .get()
+                .getName();
+    }
+
+    public static List<Ator> readData(Path path){
         List<Ator> listaAtores = new ArrayList<>();
         try{
                 List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
